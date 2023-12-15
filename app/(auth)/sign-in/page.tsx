882f49e77f4837d392f444c4ffd173ee4b340fb4
Hung Nguyen <nbhung278@gmail.com>
2023-12-15 10:13:45 +0700

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,216 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const SignIn = () => {
+  return (
+    // <div className="w-full h-screen flex flex-row items-center justify-center">
+    //   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    //     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    //       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+    //         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+    //           Sign in to your account
+    //         </h1>
+    //         <form className="space-y-4 md:space-y-6" action="#">
+    //           <div>
+    //             <label
+    //               htmlFor="email"
+    //               className="block mb-2 text-[14px] font-medium text-gray-900 dark:text-white"
+    //             >
+    //               Your email
+    //             </label>
+    //             <input
+    //               type="email"
+    //               name="email"
+    //               id="email"
+    //               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    //               placeholder="name@company.com"
+    //             />
+    //           </div>
+    //           <div>
+    //             <label
+    //               htmlFor="password"
+    //               className="block mb-2 text-[14px] font-medium text-gray-900 dark:text-white"
+    //             >
+    //               Password
+    //             </label>
+    //             <input
+    //               type="password"
+    //               name="password"
+    //               id="password"
+    //               placeholder="••••••••"
+    //               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    //             />
+    //           </div>
+    //           <div className="flex items-center justify-center">
+    //             <a
+    //               href="#"
+    //               className="text-[14px] font-medium text-primary-600 hover:underline dark:text-primary-500"
+    //             >
+    //               Forgot password?
+    //             </a>
+    //           </div>
+    //           <div className="w-full cursor-pointer text-white bg-primary-500 hover:bg-[#766dfb] font-medium rounded-lg text-sm px-5 py-2.5 text-center active:scale-95 transition ease-in-out">
+    //             Sign in
+    //           </div>
+    //           <p className="text-[14px] font-light text-gray-500 dark:text-gray-400">
+    //             Don’t have an account yet?{" "}
+    //             <a
+    //               href="#"
+    //               className="text-[14px] font-medium text-primary-600 hover:underline dark:text-primary-500"
+    //             >
+    //               Sign up
+    //             </a>
+    //           </p>
+    //         </form>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-dark-1">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md flex flex-col items-center">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
+          Welcome to Docks
+        </h2>
+        <Link href="/sign-up" className="mt-2 text-sm text-[#8b8a8a] max-w">
+          create an account
+        </Link>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your email address"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Enter your password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember_me"
+                  name="remember_me"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember_me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:text-blue-500"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 active:scale-95 transition ease-in-out "
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300"></div>
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-3 gap-3">
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 active:scale-95 transition ease-in-out"
+                >
+                  <Image
+                    width={20}
+                    height={20}
+                    src="https://www.svgrepo.com/show/512120/facebook-176.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 active:scale-95 transition ease-in-out"
+                >
+                  <Image
+                    width={20}
+                    height={20}
+                    src="https://www.svgrepo.com/show/513008/twitter-154.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 active:scale-95 transition ease-in-out"
+                >
+                  <Image
+                    width={24}
+                    height={24}
+                    src="https://www.svgrepo.com/show/506498/google.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
